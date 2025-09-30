c05a2b335c6cb686275ae8b9701cd347a06e3b23
krasimir-zhelezov <krasizh07@gmail.com>
2025-09-30 23:08:25 +0300

    feat: render pipeline

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,55 +1,107 @@
 use std::sync::Arc;
+use wgpu::{RenderPipeline, ShaderSource};
 use wgpu::{wgc::device::queue, wgt::DeviceDescriptor, Features, Instance, Limits, MemoryHints, PowerPreference, RequestAdapterOptions};
 use wgpu::{wgt::TextureViewDescriptor, Device, Queue, RequestAdapterOptionsBase, Surface, SurfaceConfiguration};
 use winit::{dpi::PhysicalSize, window::Window};
 
 pub async fn init_graphics(window: Arc<Window>) -> Option<Graphics> {
-        let instance = Instance::default();
+    let instance = Instance::default();
 
-        // let surface = Arc::new(instance.create_surface(&**window).unwrap());
+    let surface = instance.create_surface(window.clone()).unwrap();
 
-        // let window: &'static Window = Box::leak(Box::new(self.window.as_ref().unwrap().clone()));
-        // let surface = instance.create_surface(window).unwrap();
+    let adapter = instance.request_adapter(&RequestAdapterOptions {
+        power_preference: PowerPreference::default(),
+        force_fallback_adapter: false,
+        compatible_surface: Some(&surface)
+    })
+    .await
+    .expect("Could not get an open GPU adapter");
 
-        let surface = instance.create_surface(window.clone()).unwrap();
+     let (device, queue) = adapter.request_device(
+        &DeviceDescriptor {
+            label: None,
+            required_features: Features::empty(),
+            required_limits: Limits::default(),
+            memory_hints: MemoryHints::Performance,
+            trace: Default::default(),
+        }
+    )
+    .await
+    .expect("Failed to get device");
 
-        let adapter = instance.request_adapter(&RequestAdapterOptions {
-            power_preference: PowerPreference::default(),
-            force_fallback_adapter: false,
-            compatible_surface: Some(&surface)
-        })
-        .await
-        .expect("Could not get an open GPU adapter");
+    let size = window.inner_size();
+    let width = size.width.max(1);
+    let height = size.height.max(1);
+    let surface_configuration = surface.get_default_config(&adapter, width, height).unwrap();
+    surface.configure(&device, &surface_configuration);
 
-        let (device, queue) = adapter.request_device(
-            &DeviceDescriptor {
-                label: None,
-                required_features: Features::empty(),
-                required_limits: Limits::default(),
-                memory_hints: MemoryHints::Performance,
-                trace: Default::default(),
-            }
-        )
-        .await
-        .expect("Failed to get device");
-
-        let size = window.inner_size();
-        let width = size.width.max(1);
-        let height = size.height.max(1);
-        let surface_config = surface.get_default_config(&adapter, width, height).unwrap();
-        surface.configure(&device, &surface_config);
+    let render_pipeline = create_render_pipeline(&device, &surface_configuration);
         
-        let gfx= Graphics {
-            window,
-            instance,
-            surface,
-            device,
-            surface_configuration: surface_config,
-            queue
-        };
+    let gfx= Graphics {
+        window,
+        instance,
+        surface,
+        device,
+        surface_configuration,
+        queue,
+        render_pipeline
+    };
 
-        Some(gfx)
-    }
+    Some(gfx)
+}
+
+fn create_render_pipeline(device: &Device, config: &SurfaceConfiguration) -> wgpu::RenderPipeline {
+    let shader_source = ShaderSource::Wgsl(include_str!("shader.wgsl").into());
+
+    let shader_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: None,
+        source: shader_source,
+    });
+
+    let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: None,
+        bind_group_layouts: &[],
+        push_constant_ranges: &[],
+    });
+
+    device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: None,
+        layout: Some(&render_pipeline_layout),
+        vertex: wgpu::VertexState {
+            module: &shader_module,
+            entry_point: Some("vs_main"),
+            buffers: &[],
+            compilation_options: Default::default(),
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: &shader_module,
+            entry_point: Some("fs_main"),
+            targets: &[Some(wgpu::ColorTargetState {
+                format: config.format,
+                blend: Some(wgpu::BlendState::REPLACE),
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+            compilation_options: Default::default(),
+        }),
+        primitive: wgpu::PrimitiveState {
+            topology: wgpu::PrimitiveTopology::TriangleList,
+            strip_index_format: None,
+            front_face: wgpu::FrontFace::Ccw,
+            cull_mode: Some(wgpu::Face::Back),
+            unclipped_depth: false,
+            polygon_mode: wgpu::PolygonMode::Fill,
+            conservative: false,
+        },
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState {
+            count: 1,
+            mask: !0,
+            alpha_to_coverage_enabled: false,
+        },
+        multiview: None,
+        cache: None,
+    })
+}
 
 pub struct Graphics {
     pub window: Arc<Window>,
@@ -58,6 +110,7 @@ pub struct Graphics {
     pub surface_configuration: SurfaceConfiguration,
     pub device: Device,
     pub queue: Queue,
+    pub render_pipeline: RenderPipeline
 }
 
 impl Graphics {
@@ -81,7 +134,7 @@ impl Graphics {
             });
 
         {
-            encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: None,
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                     view: &view,
@@ -95,8 +148,8 @@ impl Graphics {
                 occlusion_query_set: None,
             });
 
-            // render_pass.set_pipeline(&self.render_pipeline);
-            // render_pass.draw();
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
         }
 
         self.queue.submit(Some(encoder.finish()));
