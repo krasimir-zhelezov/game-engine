0f2ee05a984586a1b1237fb2ecdb614784f8b7da
krasimir-zhelezov <krasizh07@gmail.com>
2025-10-11 20:21:14 +0300

    feat: entity manager

--- src/entities/entity_manager.rs
+++ src/entities/entity_manager.rs
@@ -1,19 +1,39 @@
-use crate::entities::entity::Entity;
+use crate::{components::{component_store::ComponentStore, tag::Tag}, entities::entity::Entity};
 
 pub struct EntityManager {
     next_id: u32,
+    entities: Vec<Entity>,
 }
 
 impl EntityManager {
     pub fn new() -> Self {
         EntityManager { 
             next_id: 0,
+            entities: Vec::new(),
         }
     }
 
     pub fn create_entity(&mut self) -> Entity {
         let entity = Entity { id: self.next_id };
+        self.entities.push(entity);
         self.next_id += 1;
         entity
+    }
+
+    pub fn get_entities_by_tag(&self, tag: &str, component_store: &ComponentStore) -> Vec<Entity> {
+        let mut tagged_entities = Vec::new();
+        for entity in &self.entities {
+            if let Some(tag) = component_store.get_component::<Tag>(entity) {
+                if tag.name == "Player" {
+                    tagged_entities.push(*entity);
+                }
+            }
+        }
+        
+        tagged_entities
+    }
+    
+    pub fn get_entities(&self) -> &Vec<Entity> {
+        &self.entities
     }
 }
--- src/world.rs
+++ src/world.rs
@@ -3,7 +3,7 @@ use std::{any::{Any, TypeId}, collections::HashMap, time::{Duration, Instant}};
 use wgpu::naga::Type;
 use winit::{event::{self, ElementState, KeyEvent}, keyboard::{self, Key, KeyCode, PhysicalKey}};
 
-use crate::{components::{component_store::ComponentStore, tag::Tag, transform::{Position, Scale, Transform}}, entities::entity::Entity, resources::resource_store::ResourceStore, systems::{input_system::{self, InputState, InputSystem}, system::System, system_manager::SystemManager}};
+use crate::{components::{component_store::ComponentStore, tag::Tag, transform::{Position, Scale, Transform}}, entities::{entity::Entity, entity_manager::EntityManager}, resources::resource_store::ResourceStore, systems::{input_system::{self, InputState, InputSystem}, system::System, system_manager::SystemManager}};
 
 pub struct World {
     pub running: bool,
@@ -13,7 +13,7 @@ pub struct World {
     pub systems: SystemManager,
     pub next_id: u32,
     pub components: ComponentStore, // Component Type to (Entity ID to Component)
-    pub entities: Vec<Entity>,
+    pub entities: EntityManager,
 }
 
 pub struct WorldView<'a> {
@@ -30,20 +30,20 @@ impl World {
             systems: SystemManager::new(),
             next_id: 0,
             components: ComponentStore::new(),
-            entities: Vec::new(),
+            entities: EntityManager::new(),
         };
 
         world.resources.insert(InputState::new());
         world.systems.add_system(Box::new(InputSystem::new()));
 
-        let player = world.create_entity();
+        let player = world.entities.create_entity();
         world.components.add_component(player, Transform {
             position: Position { x: 0.0, y: 0.0 },
             scale: Scale { x: 1.0, y: 1.0 },
         });
         world.components.add_component(player, Tag::new("Player"));
 
-        let enemy = world.create_entity();
+        let enemy = world.entities.create_entity();
         world.components.add_component(enemy, Transform {
             position: Position { x: 5.0, y: 5.0 },
             scale: Scale { x: 1.0, y: 1.0 },
@@ -59,20 +59,12 @@ impl World {
 
         let input = self.resources.get::<InputState>().unwrap();
         if input.is_key_pressed(KeyCode::KeyW) {
-            for entity in &self.entities {
-                if let Some(tag) = self.components.get_component::<Tag>(entity) {
-                    if tag.name != "Player" {
-                        continue;
-                    }
-                } else {
-                    continue;
-                }
-                
-                if let Some(transform) = self.components.get_component_mut::<Transform>(entity) {
-                    transform.position.y += 1.0;
-                    println!("Entity {} moved to position: ({}, {})", entity.id, transform.position.x, transform.position.y);
-                }
-            }            
+            let entity = self.entities.get_entities_by_tag("Player", &self.components)[0];
+
+            if let Some(transform) = self.components.get_component_mut::<Transform>(&entity) {
+                transform.position.y += 1.0;
+                println!("Entity {} moved to position: ({}, {})", entity.id, transform.position.x, transform.position.y);
+            }
         }
     }
 
@@ -94,17 +86,5 @@ impl World {
 
     pub fn handle_mouse_input(&self) {
         todo!();
-    }
-
-    pub fn create_entity(&mut self) -> Entity {
-        self.next_id += 1;
-
-        let entity = Entity {
-            id: self.next_id,
-        };
-
-        self.entities.push(entity);
-
-        entity
     }
 }
